ce889703822078c2622c7171c05d40c12b66a50a
Yoshi <ths40013@ths.hal.ac.jp>
2025-10-20 20:35:45 +0900

    player.cppの改変
    ヘッダーコメントを追加しました。

---
+++ player.cpp
@@ -0,0 +1,23 @@
+﻿/*--------------------------------------------------------------------------------
+FileName	:player.cpp.h（プレイヤー制御）
+Author		: Yoshihiro Kayamori
+Date			: 20251020 - 20251020
+--------------------------------------------------------------------------------*/
+#include "stdafx.h"
+#include "player.h"
+
+void PlayerInit()
+{
+}
+
+void PlayerUpdate()
+{
+}
+
+void PlayerDraw()
+{
+}
+
+void PlayerFinalize()
+{
+}
