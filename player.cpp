2ce43850890acb9a879dfecc8a849d52aaf1278b
Yoshi <ths40013@ths.hal.ac.jp>
2025-10-21 10:11:46 +0900

    player.cppの変更
    player初期化でプレイヤーの初期位置を設定しました

--- player.cpp
+++ player.cpp
@@ -1,8 +1,16 @@
-﻿#include "stdafx.h"
+﻿/*--------------------------------------------------------------------------------
+FileName	:player.h（プレイヤー制御）
+Author		: Yoshihiro Kayamori
+Date			: 2025/10/21 - 2025/10/21
+--------------------------------------------------------------------------------*/
+#include "stdafx.h"
 #include "player.h"
 
 void PlayerInit()
 {
+	//プレイヤー初期化
+	float playerStartX = 100.0f;
+	float playerStartY = 300.0f;
 }
 
 void PlayerUpdate()
