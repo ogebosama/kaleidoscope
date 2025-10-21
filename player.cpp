af5ba43a017cb28906c2694aa63ae40c13d144c7
zero8620 <reiji051113@gmail.com>
2025-10-21 10:09:25 +0900

    Update player.cpp
    描画処理の確定案（テスト）

--- player.cpp
+++ player.cpp
@@ -3,16 +3,20 @@
 
 void PlayerInit()
 {
+	std::cout << "ここでプレイヤーを確定させます." << std::endl;
 }
 
 void PlayerUpdate()
 {
+	std::cout << "ここでプレイヤーを変身させます." << std::endl;
 }
 
 void PlayerDraw()
 {
+	std::cout << "ここでプレイヤーを描画します." << std::endl;
 }
 
 void PlayerFinalize()
 {
+	std::cout << "ここでプレイヤーを倒します." << std::endl;
 }
